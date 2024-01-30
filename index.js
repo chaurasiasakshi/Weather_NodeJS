61689bd20d8d06d68030667b943d78ca04db9020
Sakshi Chaurasia <121746813+chaurasiasakshi@users.noreply.github.com>
2024-01-30 15:57:47 +0530

    using node

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ var requests = require("requests");
 const homeFile = fs.readFileSync("home.html","utf-8");
 const replaceVal = (tempVal,orgVal)=>{
 let temperature = tempVal.replace("{%tempval%}",orgVal.main.temp);
-temperature = tempVal.replace("{%tempmin%}",orgVal.main.temp);
-temperature = tempVal.replace("{%tempmax%}",orgVal.main.temp);
-temperature = tempVal.replace("{%location%}",orgVal.main.temp);
-temperature = tempVal.replace("{%country%}",orgVal.main.temp);
-
+temperature = tempVal.replace("{%tempmin%}",orgVal.main.temp_min);
+temperature = tempVal.replace("{%tempmax%}",orgVal.main.temp_max);
+temperature = tempVal.replace("{%location%}",orgVal.name);
+temperature = tempVal.replace("{%country%}",orgVal.sys.country);
+temperature = tempVal.replace("{%tempStatus%}",orgVal.sys.country);
+return temperature;
 }
 const server = http.createServer((req,res)=>{
     if(req.url== "/"){
@@ -18,14 +19,15 @@ requests("https://api.openweathermap.org/data/2.5/weather?q=Pune&units=metric&ap
             const objdata = JSON.parse(chunk);
             const arrData = [objdata];
             // console.log(arrData[0].main.temp);
-            const realTimeData = arrData.map((val)=>{
-               console.log(val.main);
-                // replaceVal(homeFile,val);
-            });
+            const realTimeData = arrData.map((val)=>
+            //    console.log(val.main);
+                replaceVal(homeFile,val)).join("");
+                res.write(realTimeData);
+                // console.log(realTimeData);
         })
         .on("end", (err) => {
             if (err) return console.log("connection closed due to errors", err);
-            console.log("end");
+            res.end();
         });
     }
 });
